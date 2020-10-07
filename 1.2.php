a4d39a28778797dad7c8b8370ffee0daea72e531
Дмитрий Колтаков <dyukoltakov@miem.hse.ru>
2020-10-07 03:30:01 +0300

    v2

---
+++ 1.2.php
@@ -0,0 +1,17 @@
+<?php
+function getSumNK( array $input, int $N, int $K ){
+    if(count($input)==0){return 0;}
+    if(count($input)<$K+$N){return -1;}
+    foreach($input as $val){
+        if(gettype($val) != 'integer'){return -1;}
+    }
+    if($N <= 0){return -1;}
+    if($N == 1){return $input[$K-1];}
+    $int=0;
+    foreach(range(0,$N-1) as $i){
+        $int += $input[$K+$i-1];
+    }
+    echo("$int \n");
+    return $int;
+}
+?>
